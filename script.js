a871a1c11c9afa7230d0da02a80718bc49af5148
Lang <langchenca@gmail.com>
2024-02-12 09:37:28 -0800

    Done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var selectedElectron = -1;
 var correct;
 var solved = false;
 var randomCompound;
-var score = 0;
+var score = 9;
 var compounds;
 var compoundsAnswer;
 var h2o;
@@ -16,6 +16,7 @@ var co2;
 var h2s;
 var hcn;
 var nocl;
+var done = false;
 /*var table = [
 	["H", "Li", "Na", "K", "Rb", "Cs", "Fr"],
 	["Be", "Mg", "Ca", "Sr", "Ba", "Ra"],
@@ -271,229 +272,239 @@ function setup() {
 }
 
 function draw() {
-	background("#1E1E1E");
-	noStroke();
+	if (score < 10) {
+		background("#1E1E1E");
+		noStroke();
 
-	fill("#8CED8C");
-	ellipse(width * 1 / 4, height / 2, 200, 200);
-	fill("#000000");
-	textSize(100);
-	text(element1, width * 1 / 4, height / 2);
+		fill("#8CED8C");
+		ellipse(width * 1 / 4, height / 2, 200, 200);
+		fill("#000000");
+		textSize(100);
+		text(element1, width * 1 / 4, height / 2);
 
-	fill("#8CED8C");
-	ellipse(width * 2 / 4, height / 2, 200, 200);
-	fill("#000000");
-	textSize(100);
-	text(element2, width * 2 / 4, height / 2);
-	// First atom with text
+		fill("#8CED8C");
+		ellipse(width * 2 / 4, height / 2, 200, 200);
+		fill("#000000");
+		textSize(100);
+		text(element2, width * 2 / 4, height / 2);
+		// First atom with text
 
-	fill("#8CED8C");
-	ellipse(width * 3 / 4, height / 2, 200, 200);
-	fill("#000000");
-	textSize(100);
-	text(element3, width * 3 / 4, height / 2);
-	// Second atom with text
+		fill("#8CED8C");
+		ellipse(width * 3 / 4, height / 2, 200, 200);
+		fill("#000000");
+		textSize(100);
+		text(element3, width * 3 / 4, height / 2);
+		// Second atom with text
 
-	for (x = 0; x < electrons.length; x++) {
-		for (y = 0; y < electrons[x].length; y++) {
-			if (
-				selectedElectron != -1
-				&& (electrons[x][y].x == width * 1 / 4 + 140
-					|| electrons[x][y].x == width * 3 / 4 - 140)
-			) {
-				fill("#A5D8FF")
-				ellipse(electrons[x][y].x, electrons[x][y].y, 60 * sin(frameCount / 25), 60 * sin(frameCount / 25));
-			}
+		for (x = 0; x < electrons.length; x++) {
+			for (y = 0; y < electrons[x].length; y++) {
+				if (
+					selectedElectron != -1
+					&& (electrons[x][y].x == width * 1 / 4 + 140
+						|| electrons[x][y].x == width * 3 / 4 - 140)
+				) {
+					fill("#A5D8FF")
+					ellipse(electrons[x][y].x, electrons[x][y].y, 60 * sin(frameCount / 25), 60 * sin(frameCount / 25));
+				}
 
-			fill("#FFFFFF");
-			ellipse(electrons[x][y].x, electrons[x][y].y, 40, 40);
-			fill("#000000");
-			textSize(20);
-			text(electrons[x][y].id, electrons[x][y].x, electrons[x][y].y);
+				fill("#FFFFFF");
+				ellipse(electrons[x][y].x, electrons[x][y].y, 40, 40);
+				fill("#000000");
+				textSize(20);
+				text(electrons[x][y].id, electrons[x][y].x, electrons[x][y].y);
 
-			push();
-			stroke("#FFFFFF");
-			strokeWeight(40);
-			if (electrons[x][y].bond != -1) {
-				for (var x1 = 0; x1 < electrons.length; x1++) {
-					for (var y1 = 0; y1 < electrons[x1].length; y1++) {
-						if (electrons[x1][y1].id == electrons[x][y].bond) {
-							line(electrons[x][y].x, electrons[x][y].y, electrons[x1][y1].x, electrons[x1][y1].y);
+				push();
+				stroke("#FFFFFF");
+				strokeWeight(40);
+				if (electrons[x][y].bond != -1) {
+					for (var x1 = 0; x1 < electrons.length; x1++) {
+						for (var y1 = 0; y1 < electrons[x1].length; y1++) {
+							if (electrons[x1][y1].id == electrons[x][y].bond) {
+								line(electrons[x][y].x, electrons[x][y].y, electrons[x1][y1].x, electrons[x1][y1].y);
+							}
 						}
 					}
 				}
+				pop();
 			}
-			pop();
 		}
-	}
-	// Electrons
+		// Electrons
 
-	stroke("#FFFFFF");
-	strokeWeight(40);
-	if (selectedElectron != -1) {
-		for (x = 0; x < electrons.length; x++) {
-			for (y = 0; y < electrons[x].length; y++) {
-				if (electrons[x][y].id == selectedElectron) {
-					line(electrons[x][y].x, electrons[x][y].y, mouseX, mouseY);
+		stroke("#FFFFFF");
+		strokeWeight(40);
+		if (selectedElectron != -1) {
+			for (x = 0; x < electrons.length; x++) {
+				for (y = 0; y < electrons[x].length; y++) {
+					if (electrons[x][y].id == selectedElectron) {
+						line(electrons[x][y].x, electrons[x][y].y, mouseX, mouseY);
+					}
 				}
 			}
 		}
-	}
-	strokeWeight(40);
-	stroke("#000000");
-	line(100, 100, width - 100, 100);
-	stroke("#8CED8C");
-	if (score > 0) {
-		line(100, 100, score / 10 * (width - 100), 100);
-	}
-	noStroke();
-	if (correct) {
-		fill("#8CED8C");
-	} else {
-		fill("#FF0000")
-	}
-	rectMode(CORNERS);
-	rect(0, height - 100, width, height);
-	fill("#000000");
-	if (correct) {
-		text("Correct! Press any key to continue.", width / 2, height - 50);
-		if (!solved) {
-			solved = true;
-			score++;
+		strokeWeight(40);
+		stroke("#000000");
+		line(100, 100, width - 100, 100);
+		stroke("#8CED8C");
+		if (score > 0) {
+			line(100, 100, score / 10 * (width - 100), 100);
 		}
-		if (keyIsPressed) {
-			correct = false;
-			solved = false;
-			randomCompound = Math.floor(Math.random() * compounds.length);
-			electrons = compounds[randomCompound].array
-			answer = compoundsAnswer[randomCompound].array
-			element1 = compounds[randomCompound].c1
-			element2 = compounds[randomCompound].c2
-			element3 = compounds[randomCompound].c3
-			compounds = [
-				{ name: "H2O", c1: "H", c2: "O", c3: "H", array: h2o },
-				{ name: "CO2", c1: "O", c2: "C", c3: "O", array: co2 },
-				{ name: "H2S", c1: "H", c2: "S", c3: "H", array: h2s },
-				{ name: "HCN", c1: "H", c2: "C", c3: "N", array: hcn },
-				{ name: "NOCl", c1: "O", c2: "N", c3: "Cl", array: nocl },
-			]
-			h2o = [
-				[
-					{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
-				],
-				[
-					{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
-					{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 2 },
-					{ x: width * 2 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
-					{ x: width * 2 / 4 - 50, y: height / 2 + 140, bond: -1, id: 4 },
-					{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 5 },
-					{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 6 },
-				],
-				[
-					{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 7 },
-				],
-			]
-
-			co2 = [
-				[
-					{ x: width * 1 / 4 + 50, y: height / 2 - 140, bond: -1, id: 0 },
-					{ x: width * 1 / 4 - 50, y: height / 2 - 140, bond: -1, id: 1 },
-					{ x: width * 1 / 4 + 50, y: height / 2 + 140, bond: -1, id: 2 },
-					{ x: width * 1 / 4 - 50, y: height / 2 + 140, bond: -1, id: 3 },
-					{ x: width * 1 / 4 + 140, y: height / 2 - 50, bond: -1, id: 4 },
-					{ x: width * 1 / 4 + 140, y: height / 2 + 50, bond: -1, id: 5 },
-				],
-				[
-					{ x: width * 2 / 4 - 140, y: height / 2 - 50, bond: -1, id: 6 },
-					{ x: width * 2 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
-					{ x: width * 2 / 4 + 140, y: height / 2 - 50, bond: -1, id: 8 },
-					{ x: width * 2 / 4 + 140, y: height / 2 + 50, bond: -1, id: 9 },
-				],
-				[
-					{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1, id: 10 },
-					{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1, id: 11 },
-					{ x: width * 3 / 4 + 50, y: height / 2 + 140, bond: -1, id: 12 },
-					{ x: width * 3 / 4 - 50, y: height / 2 + 140, bond: -1, id: 13 },
-					{ x: width * 3 / 4 - 140, y: height / 2 - 50, bond: -1, id: 14 },
-					{ x: width * 3 / 4 - 140, y: height / 2 + 50, bond: -1, id: 15 },
-				],
-			]
-
-			h2s = [
-				[
-					{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
-				],
-				[
-					{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
-					{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 2 },
-					{ x: width * 2 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
-					{ x: width * 2 / 4 - 50, y: height / 2 + 140, bond: -1, id: 4 },
-					{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 5 },
-					{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 6 },
-				],
-				[
-					{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 7 },
-				],
-			]
-
-			hcn = [
-				[
-					{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
-				],
-				[
-					{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 1 },
-					{ x: width * 2 / 4 + 140, y: height / 2 - 50, bond: -1, id: 2 },
-					{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 3 },
-					{ x: width * 2 / 4 + 140, y: height / 2 + 50, bond: -1, id: 4 },
-				],
-				[
-					{ x: width * 3 / 4 - 140, y: height / 2 - 50, bond: -1, id: 5 },
-					{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 6 },
-					{ x: width * 3 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
-					{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1 - 140, id: 8 },
-					{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1 - 140, id: 9 },
-				],
-			]
-
-			nocl = [
-				[
-					{ x: width * 1 / 4 - 50, y: height / 2 - 140, bond: -1, id: 0 },
-					{ x: width * 1 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
-					{ x: width * 1 / 4 - 50, y: height / 2 + 140, bond: -1, id: 2 },
-					{ x: width * 1 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
-					{ x: width * 1 / 4 + 140, y: height / 2 - 50, bond: -1, id: 4 },
-					{ x: width * 1 / 4 + 140, y: height / 2 + 50, bond: -1, id: 5 },
-				],
-				[
-					{ x: width * 2 / 4 - 140, y: height / 2 - 50, bond: -1, id: 6 },
-					{ x: width * 2 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
-					{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 8 },
-					{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 9 },
-					{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 10 },
-
-				],
-				[
-					{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1, id: 11 },
-					{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1, id: 12 },
-					{ x: width * 3 / 4 + 50, y: height / 2 + 140, bond: -1, id: 13 },
-					{ x: width * 3 / 4 - 50, y: height / 2 + 140, bond: -1, id: 14 },
-					{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 15 },
-					{ x: width * 3 / 4 + 140, y: height / 2 - 50, bond: -1, id: 16 },
-					{ x: width * 3 / 4 + 140, y: height / 2 + 50, bond: -1, id: 17 },
-				],
-			]
+		noStroke();
+		if (correct) {
+			fill("#8CED8C");
+		} else {
+			fill("#FF0000")
 		}
-	} else {
-		text("Click on the electrons around the middle atom, then click on the electrons around a nearby atom to bond the electrons.", width / 2, height - 50);
-	}
-	rectMode(CENTER);
-	correct = true;
-	for (x = 0; x < electrons.length; x++) {
-		for (y = 0; y < electrons[x].length; y++) {
-			if (answer[x][y].bond != electrons[x][y].bond) {
+		rectMode(CORNERS);
+		rect(0, height - 100, width, height);
+		fill("#000000");
+		if (correct) {
+			text("Correct! Press any key to continue.", width / 2, height - 50);
+			if (!solved) {
+				solved = true;
+				score++;
+			}
+			if (keyIsPressed) {
 				correct = false;
+				solved = false;
+				randomCompound = Math.floor(Math.random() * compounds.length);
+				electrons = compounds[randomCompound].array
+				answer = compoundsAnswer[randomCompound].array
+				element1 = compounds[randomCompound].c1
+				element2 = compounds[randomCompound].c2
+				element3 = compounds[randomCompound].c3
+				compounds = [
+					{ name: "H2O", c1: "H", c2: "O", c3: "H", array: h2o },
+					{ name: "CO2", c1: "O", c2: "C", c3: "O", array: co2 },
+					{ name: "H2S", c1: "H", c2: "S", c3: "H", array: h2s },
+					{ name: "HCN", c1: "H", c2: "C", c3: "N", array: hcn },
+					{ name: "NOCl", c1: "O", c2: "N", c3: "Cl", array: nocl },
+				]
+				h2o = [
+					[
+						{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
+					],
+					[
+						{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
+						{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 2 },
+						{ x: width * 2 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
+						{ x: width * 2 / 4 - 50, y: height / 2 + 140, bond: -1, id: 4 },
+						{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 5 },
+						{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 6 },
+					],
+					[
+						{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 7 },
+					],
+				]
+
+				co2 = [
+					[
+						{ x: width * 1 / 4 + 50, y: height / 2 - 140, bond: -1, id: 0 },
+						{ x: width * 1 / 4 - 50, y: height / 2 - 140, bond: -1, id: 1 },
+						{ x: width * 1 / 4 + 50, y: height / 2 + 140, bond: -1, id: 2 },
+						{ x: width * 1 / 4 - 50, y: height / 2 + 140, bond: -1, id: 3 },
+						{ x: width * 1 / 4 + 140, y: height / 2 - 50, bond: -1, id: 4 },
+						{ x: width * 1 / 4 + 140, y: height / 2 + 50, bond: -1, id: 5 },
+					],
+					[
+						{ x: width * 2 / 4 - 140, y: height / 2 - 50, bond: -1, id: 6 },
+						{ x: width * 2 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
+						{ x: width * 2 / 4 + 140, y: height / 2 - 50, bond: -1, id: 8 },
+						{ x: width * 2 / 4 + 140, y: height / 2 + 50, bond: -1, id: 9 },
+					],
+					[
+						{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1, id: 10 },
+						{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1, id: 11 },
+						{ x: width * 3 / 4 + 50, y: height / 2 + 140, bond: -1, id: 12 },
+						{ x: width * 3 / 4 - 50, y: height / 2 + 140, bond: -1, id: 13 },
+						{ x: width * 3 / 4 - 140, y: height / 2 - 50, bond: -1, id: 14 },
+						{ x: width * 3 / 4 - 140, y: height / 2 + 50, bond: -1, id: 15 },
+					],
+				]
+
+				h2s = [
+					[
+						{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
+					],
+					[
+						{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
+						{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 2 },
+						{ x: width * 2 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
+						{ x: width * 2 / 4 - 50, y: height / 2 + 140, bond: -1, id: 4 },
+						{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 5 },
+						{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 6 },
+					],
+					[
+						{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 7 },
+					],
+				]
+
+				hcn = [
+					[
+						{ x: width * 1 / 4 + 140, y: height / 2, bond: -1, id: 0 },
+					],
+					[
+						{ x: width * 2 / 4 - 140, y: height / 2, bond: -1, id: 1 },
+						{ x: width * 2 / 4 + 140, y: height / 2 - 50, bond: -1, id: 2 },
+						{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 3 },
+						{ x: width * 2 / 4 + 140, y: height / 2 + 50, bond: -1, id: 4 },
+					],
+					[
+						{ x: width * 3 / 4 - 140, y: height / 2 - 50, bond: -1, id: 5 },
+						{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 6 },
+						{ x: width * 3 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
+						{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1 - 140, id: 8 },
+						{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1 - 140, id: 9 },
+					],
+				]
+
+				nocl = [
+					[
+						{ x: width * 1 / 4 - 50, y: height / 2 - 140, bond: -1, id: 0 },
+						{ x: width * 1 / 4 + 50, y: height / 2 - 140, bond: -1, id: 1 },
+						{ x: width * 1 / 4 - 50, y: height / 2 + 140, bond: -1, id: 2 },
+						{ x: width * 1 / 4 + 50, y: height / 2 + 140, bond: -1, id: 3 },
+						{ x: width * 1 / 4 + 140, y: height / 2 - 50, bond: -1, id: 4 },
+						{ x: width * 1 / 4 + 140, y: height / 2 + 50, bond: -1, id: 5 },
+					],
+					[
+						{ x: width * 2 / 4 - 140, y: height / 2 - 50, bond: -1, id: 6 },
+						{ x: width * 2 / 4 - 140, y: height / 2 + 50, bond: -1, id: 7 },
+						{ x: width * 2 / 4 + 140, y: height / 2, bond: -1, id: 8 },
+						{ x: width * 2 / 4 - 50, y: height / 2 - 140, bond: -1, id: 9 },
+						{ x: width * 2 / 4 + 50, y: height / 2 - 140, bond: -1, id: 10 },
+
+					],
+					[
+						{ x: width * 3 / 4 + 50, y: height / 2 - 140, bond: -1, id: 11 },
+						{ x: width * 3 / 4 - 50, y: height / 2 - 140, bond: -1, id: 12 },
+						{ x: width * 3 / 4 + 50, y: height / 2 + 140, bond: -1, id: 13 },
+						{ x: width * 3 / 4 - 50, y: height / 2 + 140, bond: -1, id: 14 },
+						{ x: width * 3 / 4 - 140, y: height / 2, bond: -1, id: 15 },
+						{ x: width * 3 / 4 + 140, y: height / 2 - 50, bond: -1, id: 16 },
+						{ x: width * 3 / 4 + 140, y: height / 2 + 50, bond: -1, id: 17 },
+					],
+				]
+			}
+		} else {
+			text("Click on the electrons around the middle atom, then click on the electrons around a nearby atom to bond the electrons.", width / 2, height - 50);
+		}
+		rectMode(CENTER);
+		correct = true;
+		for (x = 0; x < electrons.length; x++) {
+			for (y = 0; y < electrons[x].length; y++) {
+				if (answer[x][y].bond != electrons[x][y].bond) {
+					correct = false;
+				}
 			}
 		}
+	} else {
+		if (!done) {
+			time = Math.round(millis() / 1000);
+			done = true;
+		}
+		background("#1E1E1E");
+		fill("#FFFFFF");
+		text("Congratulations! Your time was " + time + " seconds", width / 2, height / 2);
 	}
 }
 
