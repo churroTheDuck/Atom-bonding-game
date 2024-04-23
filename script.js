0658c06b8f862bc8e54c6213b5a3c8c1cf777fd1
churroTheDuck <152838803+churroTheDuck@users.noreply.github.com>
2024-04-23 16:01:25 -0700

    Added decomposition

--- script.js
+++ script.js
@@ -13,19 +13,45 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+var reactionTypes = [
+  "S",
+  "D",
+  "SR",
+  "DR",
+]
+var reactionType = reactionTypes[Math.floor(Math.random() * ((reactionTypes.length - 1) - 0 + 1) + 0)];
 var elements = [
     "P", "H", "C", "S", "N", "O"
 ]
-
 var element1 = elements[Math.floor(Math.random() * ((elements.length - 1) - 0 + 1) + 0)];
 elements.splice(elements.indexOf(element1), 1);
 var element2 = elements[Math.floor(Math.random() * ((elements.length - 1) - 0 + 1) + 0)];
+elements.splice(elements.indexOf(element2), 1);
+var element3 = elements[Math.floor(Math.random() * ((elements.length - 1) - 0 + 1) + 0)];
 elements = [
     "P", "H", "C", "S", "N", "O"
 ]
-if (elements.indexOf(element1) > elements.indexOf(element2)) {
-    var compound = element2 + " + " + element1 + " → " + element2 + element1;
-} else {
-    var compound = element1 + " + " + element2 + " → " + element1 + element2;
+switch (reactionType) {
+  case "S":
+    if (elements.indexOf(element1) > elements.indexOf(element2)) {
+        var compound = element2 + " + " + element1 + " → " + element2 + element1;
+    } else {
+        var compound = element1 + " + " + element2 + " → " + element1 + element2;
+    }
+  break;
+  case "D":   
+    if (elements.indexOf(element1) > elements.indexOf(element2)) {
+      var compound = element2 + element1 + " → " + element2 + " + " + element1;
+  } else {
+      var compound = element1 + element2 + " → " + element1 + " + " + element2;
+  }
+  break;
+  case "SR":
+    if (elements.indexOf(element1) > elements.indexOf(element2)) {
+      var compound = element2 + element1 + " → " + element2 + " + " + element1;
+  } else {
+      var compound = element1 + element2 + " → " + element1 + " + " + element2;
+  }
+  break;
 }
 document.getElementById("equation").innerHTML = compound;
